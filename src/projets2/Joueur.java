39078b24677a7fd55a302867b8a76061cb81aa36
bogae004 <bogae004@pc041.iut-rcc-info.urca>
2015-06-17 16:48:47 +0200

    Ennemi deja fini, 2 methodes de joueur necessitant l'implementation de clés.

--- src/projets2/Joueur.java
+++ src/projets2/Joueur.java
@@ -16,6 +16,7 @@ public class Joueur extends Personnage{
 	 */
 	public Joueur(Point position,int pdvMax){
 		super(position,pdvMax);
+		inventaire= new LinkedList<>();
 	}
 	
 	/**
@@ -23,16 +24,19 @@ public class Joueur extends Personnage{
 	 * @return (LinkedList) une liste remplie seulement de Ramassable
 	 */
 	public LinkedList<Ramassable> getInventaire(){
-		return null;
+		return new LinkedList<Ramassable>(this.inventaire);
 	}
 	
 	/**
-	 * Permet d'ajoute run objet a l'inventaire du Joueur
+	 * Permet d'ajouter un objet a l'inventaire du Joueur
 	 * @param entite l'objet a ajouter
 	 * @return (boolean) Savoir si l'objet a été ajouté avec succés
 	 */
 	public boolean ramasserObjet(Ramassable entite){
-		return false;
+		boolean b=false;
+		this.inventaire.add(entite);
+		b=true;
+		return b;
 	}
 	
 	/**
@@ -42,7 +46,14 @@ public class Joueur extends Personnage{
 	 * @param reference un emplacement dans la liste chainée Inventaire
 	 */
 	public void utiliserObjet(int reference){
-
+		Ramassable e=this.inventaire.get(reference);
+		if(!(e instanceof Outil)){
+			((Applicable) e).appliquerEffet(this);
+			inventaire.remove(reference);
+		}else{
+			//valentin c'est tes clés c'est ton bordel
+		}
+	
 	}
 	
 	/**
@@ -51,6 +62,35 @@ public class Joueur extends Personnage{
 	 * @return un boolean pour savoir si c'est portes ouvertes aujourdhui
 	 */
 	public boolean ouvrirPorte(Porte porte){
-		return false;
+		Boolean b=false;
+		//Valentin a toi de jouer
+		return b;
+	}
+	
+	public static void main(String[] args){
+		Joueur J=new Joueur(new Point(1,1),5);
+		/*
+		 * System.out.println(J.deplacer(200, true));
+		 
+		System.out.println(J.deplacer(208, true));
+		System.out.println(J.deplacer(205, true));
+		System.out.println(J.deplacer(203, true));
+		System.out.println(J.deplacer(200, false));
+		System.out.println("pdvmax"+J.pointsDeVieMax);
+		J.setPointsDeVieMax(9);
+		System.out.println("pdvmax"+J.pointsDeVieMax);
+		System.out.println("pdv"+J.pointsDeVie);
+		J.modifierPointsDeVie(1);
+		System.out.println("pdv"+J.pointsDeVie);
+		J.modifierPointsDeVie(10);
+		System.out.println("pdv"+J.pointsDeVie+"censé etre 9");
+		System.out.println(J.modifierPointsDeVie(-9));
+		System.out.println("pdv"+J.pointsDeVie);
+		System.out.println(J.getEtageCourant());
+		System.out.println(J.getOrientation());
+		
+	Outil o=new Outil(20);
+		J.ramasserObjet(o);
+		System.out.println(((Entite)J.getInventaire().get(0)).getReference());*/	
 	}
 }
--- src/projets2/Personnage.java
+++ src/projets2/Personnage.java
@@ -4,16 +4,27 @@ import java.awt.Point;
 
 public abstract class Personnage extends Affichable{
 	protected Point position;
+	
 	protected int pointsDeVie;
+	
 	protected int pointsDeVieMax;
+	/** ints de slick 2D
+	 * 	BAS 208
+ 	 *  HAUT 200
+	 *  GAUCHE 203 
+	 *  DROITE 205
+	 */
 	protected int orientation;
+	
 	protected int etageCourant;
+	
 	/**
 	 * Constructeur 
 	 * orientation initialisée par défaut
 	 * pointsDeVie initialisé a pointsDeVieMax
 	 * etageCourant initialisé a 0
 	 * L'image est initialisé a selon la classe (a implementer plus tard.)
+	 * 
 	 * @param position Point pour initialiser position
 	 * @param pdvMax int pour decider du nombre de points de vie  maximum 
 	 */
@@ -21,58 +32,105 @@ public abstract class Personnage extends Affichable{
 		this.position= new Point(position);
 		this.pointsDeVie=pdvMax;
 		this.pointsDeVieMax=pdvMax;
-		this.orientation=0;
+		this.orientation=205;
 		this.etageCourant=0;
 	}
 	
 	/**
 	 * Retourne la position du Personnage
+	 * 
 	 * @return (point)La position du Personnage 
 	 */
 	public Point getPosition(){
-		return null;
+		return new Point(this.position);
 	}
 	
 	/**
 	 * Renvoye les points de vie maximums
+	 * 
 	 * @return (int) Les points de vie max
 	 */
 	public int getPointDeVieMax(){
-		return 0;
+		return pointsDeVieMax;
 	}
 	
 	/**
 	 * Retourne le sens dans lequel le Personnage est tourné
+	 * 
 	 * @return (int) int representant l'orientation du personnage
 	 */
 	public int getOrientation(){
-		return 0;
+		return orientation;
+	}
+	
+	/**
+	 * Retourne l'etage actuel du personnage
+	 * 
+	 * @return l'etage actuel
+	 */
+	public int getEtageCourant(){
+		return etageCourant;
 	}
 	
 	/**
 	 * Permet de changer les points de vie avec un int tel que pointsDeVie=pointsDeVie-nbPointsDeVie
 	 * Renvoye false quand pointsDeVie apsse en dessous de 0
+	 * 
 	 * @param nbPointsDeVie (int) nombre de points de vie ajouté ou retiré 
 	 * @return (boolean) Faux si Personnage mort
 	 */
 	public boolean modifierPointsDeVie(int nbPointsDeVie){
-		return false;
+		boolean b=true;
+		if(this.pointsDeVie+nbPointsDeVie<this.pointsDeVieMax){
+		this.pointsDeVie=this.pointsDeVie+nbPointsDeVie;
+		}else{
+			this.pointsDeVie=this.pointsDeVieMax;
+		}
+		if(this.pointsDeVie<=0){
+			b=false;
+		}
+		return b;
 	}
 	
 	/**
 	 * Permet de changer la limite de points de vie
+	 * 
 	 * @param newPDVMax (int) les nouveaux points de vie maximum
 	 */
-	public void setPointsDeVieMax(int newPDVMax){}
+	public void setPointsDeVieMax(int newPDVMax){
+		this.pointsDeVieMax=newPDVMax;
+	}
 	
 	/**
 	 * Tourne le joueur dans une direction et le fait avancer ou non en fonction des parametres.
+	 * 
 	 * @param direction (int) code pour le sens dans lequel le Personnage se tourne et se déplace
 	 * @param deplacementPossible (boolean) boolean indiquant si le personnage va su la case vers laquelle il est orienté
 	 * @return La nouvelle position du joueur
 	 */
 	public Point deplacer(int direction,boolean deplacementPossible){
-		return null;
+		try{
+			if(direction!=203&&direction!=200&&direction!=205&&direction!=208){
+				throw new Exception("direction non valable");
+			}
+			this.orientation=direction;
+		if(deplacementPossible){
+			if(direction==203){//left
+			  position.translate(-1,0);
+			 }if(direction==205){//right
+			  position.translate(1,0);
+			 }if(direction==208){//bottom
+			  position.translate(0,1);
+			  }if(direction==200){//up
+			 position.translate(0,-1);
+			  }}
+			 
+		}catch(Exception e){
+			System.out.println("La direction ne correspond pas a un int des touches");
+		}
+		
+		Point p=new Point(position);
+		return p;
 	}
 	
 	
