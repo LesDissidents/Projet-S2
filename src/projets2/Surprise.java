c6693aa1d0a1ada780c5aa7fce7f34294a1d5ae5
donin002 <donin002@pc044.iut-rcc-info.urca>
2015-06-17 14:23:38 +0200

    Creation des prototypes et java doc de la classe Surprise et de l'enum Effets

---
+++ src/projets2/Surprise.java
@@ -0,0 +1,15 @@
+package projets2;
+
+public class Surprise {
+	
+	public int identifiant;
+	
+	/**
+	 * Constructeur de l'objet Surprise prenant un identifiant en paramètre
+	 * @param id - Identifiant de la surpise
+	 */
+	public Surprise(int id){
+		
+	}
+
+}
