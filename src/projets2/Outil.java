b5b5fb00be0158dce10d27aac78effdcc326295b
renau032 <renau032@pc046.iut-rcc-info.urca>
2015-06-17 15:07:44 +0200

    Implémentation des classes Applicable et Outil.

--- src/projets2/Outil.java
+++ src/projets2/Outil.java
@@ -12,4 +12,13 @@ public class Outil extends Entite implements Ramassable
 		
 	}
 	
+	/**
+	 * Méthode déterminant si un objet est ramassable ou non
+	 * @return - "true" si l'objet est ramassable, "false" sinon
+	 */
+	public boolean isRamassable()
+	{
+		
+	}
+	
 }
