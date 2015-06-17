a3b74a1f4e5da71ea32712ba31131a0f6b5bfdb7
renau032 <renau032@pc046.iut-rcc-info.urca>
2015-06-17 14:35:06 +0200

    Classe abstraite Applicable Classe Outil Interface Ramassable
    ~

---
+++ src/projets2/Outil.java
@@ -0,0 +1,15 @@
+package projets2;
+
+public class Outil
+{
+	
+	/**
+	 * Constructeur de la classe Outil permettant de créer un objet Outil à partir d'un identifiant
+	 * @param identifiant - identifiant de l'outil à créer
+	 */
+	public Outil(int identifiant)
+	{
+		
+	}
+	
+}
