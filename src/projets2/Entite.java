23cf2a4f52f5ae9c3221002a6db6a246c8db1dbb
mezen002 <mezen002@pc043.iut-rcc-info.urca>
2015-06-18 09:16:28 +0200

    indentation refaite

--- src/projets2/Entite.java
+++ src/projets2/Entite.java
@@ -9,7 +9,7 @@ public class Entite {
 	 * Constructeur d'une entitée
 	 * @param id - id de l'entité
 	 */
-	public Entite(int id){
+	public Entite(int id) {
 		this.clef = id/100;
 		this.reference = id % 100;
 	}
@@ -19,8 +19,7 @@ public class Entite {
 	 * Accesseur à la clef  de l'entité.
 	 * @return - la clef  de l'entité.
 	 */
-	public int getClef()
-	{
+	public int getClef() {
 		return this.clef;
 	}
 	
@@ -28,9 +27,8 @@ public class Entite {
 	 * Accesseur a la référence de l'entité
 	 * @return la valeur de la référence de l'entité
 	 */
-	public int getReference(){
+	public int getReference() {
 		return this.reference;
-
-}
+	}
 }
 	
