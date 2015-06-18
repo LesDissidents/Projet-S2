d1b9b3d73acc823d9117171b5b63cb46c032c57d
mezen002 <mezen002@pc043.iut-rcc-info.urca>
2015-06-18 09:14:11 +0200

    Implementation. Reste deplacer() à faire.

--- src/projets2/Personnage.java
+++ src/projets2/Personnage.java
@@ -30,7 +30,7 @@ public abstract class Personnage extends Affichable{
 	 * @return (point)La position du Personnage 
 	 */
 	public Point getPosition(){
-		return null;
+		return this.position;
 	}
 	
 	/**
@@ -38,7 +38,7 @@ public abstract class Personnage extends Affichable{
 	 * @return (int) Les points de vie max
 	 */
 	public int getPointDeVieMax(){
-		return 0;
+		return this.pointsDeVieMax;
 	}
 	
 	/**
@@ -46,7 +46,7 @@ public abstract class Personnage extends Affichable{
 	 * @return (int) int representant l'orientation du personnage
 	 */
 	public int getOrientation(){
-		return 0;
+		return this.orientation;
 	}
 	
 	/**
@@ -56,14 +56,18 @@ public abstract class Personnage extends Affichable{
 	 * @return (boolean) Faux si Personnage mort
 	 */
 	public boolean modifierPointsDeVie(int nbPointsDeVie){
-		return false;
+		this.pointsDeVie += nbPointsDeVie;
+		if(this.pointsDeVie <= 0) return false;
+		else return true;
 	}
 	
 	/**
 	 * Permet de changer la limite de points de vie
 	 * @param newPDVMax (int) les nouveaux points de vie maximum
 	 */
-	public void setPointsDeVieMax(int newPDVMax){}
+	public void setPointsDeVieMax(int newPDVMax){
+		this.pointsDeVie = newPDVMax;
+	}
 	
 	/**
 	 * Tourne le joueur dans une direction et le fait avancer ou non en fonction des parametres.
@@ -72,6 +76,7 @@ public abstract class Personnage extends Affichable{
 	 * @return La nouvelle position du joueur
 	 */
 	public Point deplacer(int direction,boolean deplacementPossible){
+		
 		return null;
 	}
 	
